7389f39be56317ee6cea9d9d2d7df5d8066bd3c5
Luke A. Smith <112637579+lasmit17@users.noreply.github.com>
2023-02-26 22:37:56 +0100

    Final commit of Assignment 3

--- hanoi.py
+++ hanoi.py
@@ -1,14 +1,11 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Feb 20 08:24:20 2023
-
-@author: Hannah
-"""
-
 def TowerOfHanoi(n , from_rod, to_rod, aux_rod): 
-    
-    #delete the pass and replace with your recursive algorithm
-    pass
-
+    #If there's only one disk, you can move it directly to the final rod and be done
+    if n == 1:
+        print(f"Move disk 1 from {from_rod} to {to_rod}")
+    else:
+        TowerOfHanoi(n-1, from_rod, aux_rod, to_rod)
+        print(f"Move disk {n} from {from_rod} to {to_rod}")
+        TowerOfHanoi(n-1, aux_rod, to_rod, from_rod)
+    #If n is greater than 1, the function will stop once the else statement reduces n to 1
 
 TowerOfHanoi(3, 'A', 'C', 'B')  
